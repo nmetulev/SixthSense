6b622a0a85f16f78a8315cb92be77de55dbc689d
Nikola Metulev <nmetulev@uci.edu>
2012-02-29 16:43:01 -0800

    UART Update

---
+++ SixthSense/USART.c
@@ -0,0 +1,6 @@
+/*
+ * USART.c
+ *
+ * Created: 2/28/2012 7:16:04 PM
+ *  Author: Laptop
+ */ 
---
+++ SixthSense/USART.h
@@ -0,0 +1,16 @@
+/*
+ * USART.h
+ *
+ * Created: 2/28/2012 7:16:19 PM
+ *  Author: Laptop
+ */ 
+
+
+#ifndef USART_H_
+#define USART_H_
+
+
+
+
+
+#endif /* USART_H_ */
